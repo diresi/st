84a47cbb3dc553342bdb625a3f172e2c8ffd0927
Christoph Rissner <resi@rissner.net>
2019-02-25 21:01:41 +0100

    fontsize 11

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminess Powerline:size=9";
+static char *font = "Terminess Powerline:size=11";
 static int borderpx = 2;
 
 /*
